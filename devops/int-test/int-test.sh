6cb6467854bf1b096a2ab98673135ec93b177034
Maksimelyan Tamashevich <maksimelyan_tamashevich@epam.com>
2022-11-22 12:10:00 +0200

    Update int test script

--- devops/int-test/int-test.sh
+++ devops/int-test/int-test.sh
@@ -3,10 +3,15 @@
 set -ex
 
 simple_app_url="http://$(cat terraform_output.txt | awk '{ print $3 }' | sed 's/"//g')"
-simple_app_response=$(curl  $simple_app_url -s)
 
-if [[ "$simple_app_response" != "Hello World!" ]]
+#simple_app_response=$(curl  $simple_app_url -s)
+simple_app_response_code=$(curl $simple_app_url -s --output curl_output.txt --write-out "%{http_code}")
+simple_app_response="$(cat curl_output.txt)"
+
+if [[ "$simple_app_response" != "Hello World!" ]] && [[ $simple_app_response_code -eq 200 ]]
   then
-    echo $simple_app_response
+    echo "Test faild. Response $simple_app_response with code && $simple_app_response_code"
     exit 1
+  else
+    echo "Test passed. Response $simple_app_response with code && $simple_app_response_code"
 fi
