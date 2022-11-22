3433a6f79b27ecd608e7db040c133ecf3f79a286
Maksimelyan Tamashevich <maksimelyan_tamashevich@epam.com>
2022-11-22 11:48:30 +0200

    Update int-test script

--- devops/int-test/int-test.sh
+++ devops/int-test/int-test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+set -ex
+
 simple_app_url="http://$(cat terraform_output.txt | awk '{ print $3 }' | sed 's/"//g')"
 simple_app_response=$(curl  $simple_app_url -s)
 
-if "$simple_app_response" != "Hello World!"
+if [[ "$simple_app_response" != "Hello World!" ]]
   then
     echo $simple_app_response
     exit 1
